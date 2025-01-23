c8fb94b9f5e2e79f7360a6b93b1c90b4bad10ab8
Dmitriy Gorokhov <dgorokhov123@gmail.com>
2025-01-23 17:22:00 +0300

    fix: added Demo menu in Main

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,36 @@ import manager.*;
 import task.*;
 
 import java.io.File;
+import java.util.NoSuchElementException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int choice = 1;
-
-        if (choice == 0)  fileBackedMakeFile();
-        if (choice == 1)  fileBackedDemo();
-        if (choice == 2)  inMemoryDemo();
-        if (choice == 3)  continuousDemo();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("######################################################################################");
+            System.out.println("Введите число и выберите демо:");
+            System.out.println("1 - InMemoryTaskManager: add(), update(), remove(), removeAll + история");
+            System.out.println("2 - InMemoryHistoryManager: поведение истории с большим объемом случайных операций");
+            System.out.println("3 - FileBackedTaskManager: генерируем данные и сохраняем в файл");
+            System.out.println("4 - FileBackedTaskManager: загрузка из файла, добавление строки и сохранение в файл");
+            System.out.println("0 - Выход");
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (NoSuchElementException | IllegalStateException e) {
+                choice = -1;
+                scanner.nextLine();
+            }
+            if      (choice == 0)  return;
+            else if (choice == 1)  inMemoryDemo();
+            else if (choice == 2)  continuousDemo();
+            else if (choice == 3)  fileBackedMakeFile();
+            else if (choice == 4)  fileBackedDemo();
+            else System.out.println("Вы ввели что-то не то, давайте еще разок!");
+        }
     }
 
     private static void inMemoryDemo() {
@@ -99,8 +118,8 @@ public class Main {
                 "\"title\":\"Наладить жизнь в России\",\"description\":\"\",\"status\":\"NEW\"}", Epic.class));
         tMan.add(gson.fromJson("{\"epicId\":" + e1.getId() + ",\"id\":0,\"title\":\"Запретить все плохое\"," +
                 "\"description\":\"Составить реестр плохого, принять законы\",\"status\":\"DONE\"}", Subtask.class));
-        tMan.add(gson.fromJson("{\"epicId\":" + e1.getId() + ",\"id\":0,\"title\":\"Избрать Трампа\"," +
-                "\"description\":\"Сделать Дони президентом США\",\"status\":\"DONE\"}", Subtask.class));
+        tMan.add(gson.fromJson("{\"epicId\":" + e1.getId() + ",\"id\":0,\"title\":\"Увеличить рождаемость\"," +
+                "\"description\":\"вернуть в страну Павла Дурова\",\"status\":\"DONE\"}", Subtask.class));
         tMan.add(gson.fromJson("{\"epicId\":" + e1.getId() + ",\"id\":0,\"title\":\"Укрепить рубль\"," +
                 "\"description\":\"и уменьшить ключевую ставку\",\"status\":\"NEW\"}", Subtask.class));
 
@@ -119,10 +138,10 @@ public class Main {
         tMan.update(new Epic(e3.getId(), "Стать крутым прогером", "план надежный как швейцарские часы"));
         tMan.update(new Subtask(e3s1.getId(), "Закончить курсы", "например Яндекс Практикум", TaskStatus.IN_PROGRESS));
         tMan.update(new Subtask(e3s2.getId(), "Стать джуном", "найти любую работу", TaskStatus.NEW));
-        tMan.update(new Subtask(e3s3.getId(), "Стать мидлом", "найти работу за деньги", TaskStatus.NEW));
+        tMan.update(new Subtask(e3s3.getId(), "Стать мидлом", "найти работу за хорошие деньги", TaskStatus.NEW));
         tMan.removeById(e3s4.getId());
         tMan.update(gson.fromJson("{\"epicId\":0,\"id\":" + e3s5.getId() + ",\"title\":\"Стать сеньором\"," +
-                "\"description\":\"и уехать на Бали\",\"status\":\"NEW\"}", Subtask.class));
+                "\"description\":\"и уехать в долину\",\"status\":\"NEW\"}", Subtask.class));
 
 
         showAll(tMan);
