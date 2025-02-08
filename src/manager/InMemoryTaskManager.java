df6c2937b8ecbad86bae234d64efa8725070cad4
Dmitriy Gorokhov <dgorokhov123@gmail.com>
2025-02-08 16:56:28 +0300

    feat: added time functionality to InMemoryTaskManager

--- src/manager/InMemoryTaskManager.java
+++ src/manager/InMemoryTaskManager.java
@@ -1,9 +1,9 @@
 package manager;
 import task.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.*;
 
 /**
  * Task manager containing all the data in RAM.
@@ -13,6 +13,7 @@ public class InMemoryTaskManager implements TaskManager {
     protected final LinkedHashMap<Integer, Task> tasks = new LinkedHashMap<>();
     protected final TaskFactory taskFactory;
     protected final HistoryManager historyManager;
+    protected final Set<Task> sortedTasks = new TreeSet<>(Comparator.comparing(Task::getStartTime).thenComparing(Task::getId));
 
     public InMemoryTaskManager(TaskFactory taskFactory, HistoryManager historyManager) {
         if (taskFactory == null) throw new IllegalArgumentException("Parameter 'taskFactory' cannot be null");
@@ -31,33 +32,38 @@ public class InMemoryTaskManager implements TaskManager {
         return task;
     }
 
+    protected List<Task> getAllRecords() {
+        return tasks.values().stream().toList();
+    }
+
     @Override
     public List<Task> getTasks() {
-        List<Task> result = new ArrayList<>();
-        for (Task task : tasks.values()) {
-            if (task instanceof Subtask || task instanceof Epic) continue;
-            result.add(task);
-        }
-        return result;
+        return tasks.values().stream()
+                .filter( t -> !(t instanceof Epic) && !(t instanceof Subtask) )
+                .toList();
     }
 
     @Override
     public List<Epic> getEpics() {
-        List<Epic> result = new ArrayList<>();
-        for (Task task : tasks.values()) {
-            if (task instanceof Epic epic)  result.add(epic);
-        }
-        return result;
+        return tasks.values().stream()
+                .filter( t -> (t instanceof Epic) )
+                .map(t -> (Epic) t)
+                .toList();
     }
 
     @Override
     public List<Subtask> getSubTasks(int epicId) {
-        List<Subtask> result = new ArrayList<>();
-        Task task = tasks.get(epicId);
-        if (task instanceof Epic epic) {
-            result.addAll(epic.getSubtasks().values());
-        }
-        return result;
+        return tasks.values().stream()
+                .filter( t -> (t instanceof Subtask) )
+                .map(t -> (Subtask) t)
+                .filter(s -> s.getEpic() != null)
+                .filter(s -> s.getEpic().getId() == epicId)
+                .toList();
+    }
+
+    @Override
+    public List<Task> getPrioritizedTasks() {
+        return sortedTasks.stream().toList();
     }
 
     public List<Task> getHistory() {
@@ -70,8 +76,10 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public Task add(Task task) {
         if (task == null) return null;
+        if (hasIntersections(task)) reArrangeTask(task);
         Task newTask = taskFactory.newTask(task);
         tasks.put(newTask.getId(), newTask);
+        if (newTask.getStartTime() != null) sortedTasks.add(newTask);
         return newTask;
     }
 
@@ -86,12 +94,14 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public Subtask add(Subtask subtask) {
         if (subtask == null) return null;
+        if (hasIntersections(subtask)) reArrangeTask(subtask);
         Task eTask = tasks.get(subtask.getEpicId());
         if (eTask == null) return null;
         if (eTask instanceof Epic epic) {
             Subtask newSubtask = taskFactory.newSubtask(subtask);
             epic.linkSubtask(newSubtask);
             tasks.put(newSubtask.getId(), newSubtask);
+            if (newSubtask.getStartTime() != null) sortedTasks.add(newSubtask);
             return newSubtask;
         }
         return null;
@@ -100,10 +110,17 @@ public class InMemoryTaskManager implements TaskManager {
     @Override
     public Task update(Task task) {
         if (task == null)  return null;
+        if (hasIntersections(task)) reArrangeTask(task);
         if (!tasks.containsKey(task.getId()))  return null;
         Task foundTask = tasks.get(task.getId());
-        if (foundTask.update(task))  return foundTask;
-        return null;
+        if (foundTask instanceof Epic) {
+            foundTask.update(task);
+        } else {
+            sortedTasks.remove(foundTask);
+            foundTask.update(task);
+            if (foundTask.getStartTime() != null) sortedTasks.add(foundTask);
+        }
+        return foundTask;
     }
 
 
@@ -116,10 +133,14 @@ public class InMemoryTaskManager implements TaskManager {
         if (task instanceof Epic epic) {
             for (Subtask subtask : epic.getSubtasks().values()) {
                 historyManager.remove(subtask.getId());
+                sortedTasks.remove(subtask);
                 tasks.remove(subtask.getId());
             }
         } else if (task instanceof Subtask subtask) {
             if (subtask.getEpic() != null)  subtask.getEpic().unlinkSubtask(id);
+            sortedTasks.remove(task);
+        } else {
+            sortedTasks.remove(task);
         }
         historyManager.remove(id);
         tasks.remove(id);
@@ -135,6 +156,11 @@ public class InMemoryTaskManager implements TaskManager {
             historyManager.remove(task.getId());
             taskIterator.remove();
         }
+        sortedTasks.clear();
+        sortedTasks.addAll(tasks.values().stream()
+                .filter(t -> !(t instanceof Epic))
+                .filter(t -> t.getStartTime() != null)
+                .toList());
     }
 
     @Override
@@ -148,6 +174,11 @@ public class InMemoryTaskManager implements TaskManager {
                 taskIterator.remove();
             }
         }
+        sortedTasks.clear();
+        sortedTasks.addAll(tasks.values().stream()
+                .filter(t -> !(t instanceof Epic))
+                .filter(t -> t.getStartTime() != null)
+                .toList());
     }
 
     @Override
@@ -159,6 +190,60 @@ public class InMemoryTaskManager implements TaskManager {
                 historyManager.remove(task.getId());
                 taskIterator.remove();
             }
+        }
+        sortedTasks.clear();
+        sortedTasks.addAll(tasks.values().stream()
+                .filter(t -> !(t instanceof Epic))
+                .filter(t -> t.getStartTime() != null)
+                .toList());
+    }
+
+    //#################################### Date Time methods ####################################
+
+    @Override
+    public boolean hasIntersections(Task task) {
+        if (task == null || task.getStartTime() == null) return false;
+        return sortedTasks.stream()
+          .anyMatch(t -> t.getStartTime().isBefore(task.getEndTime()) && task.getStartTime().isBefore(t.getEndTime()));
+    }
+
+    protected void reArrangeTask(Task task) {
+        if (task == null || task.getStartTime() == null) return;
+        List<Task> intersections;
+        LocalDateTime start, end;
+        int dayShift = 0;
+        int minuteShift = 0;
+
+        while (true) {
+            LocalDateTime finalStart = start = task.getStartTime().plusDays(dayShift).plusMinutes(minuteShift);
+            LocalDateTime finalEnd = end = task.getEndTime().plusDays(dayShift).plusMinutes(minuteShift);
+            intersections = sortedTasks.stream()
+                    .filter(t -> t.getStartTime().isBefore(finalEnd) && finalStart.isBefore(t.getEndTime()))
+                    .toList();
+            if (intersections.isEmpty()) {
+                task.setTiming(start, end);
+                return;
+            }
+            if (Duration.between(start, end).toHours() > 12) {
+                LocalDateTime lastPoint = intersections.stream()
+                        .map(Task::getEndTime)
+                        .max(Comparator.naturalOrder())
+                        .get();
+                dayShift += (int) Duration.between(start, lastPoint).toDays() + 1;
+                continue;
+            }
+            boolean ok = false;
+            while (end.getHour() < 21) {
+                minuteShift += 15;
+                LocalDateTime finalStart2 = start = start.plusMinutes(15);
+                LocalDateTime finalEnd2 = end = end.plusMinutes(15);
+                ok = intersections.stream()
+                        .noneMatch(t -> t.getStartTime().isBefore(finalEnd2) && finalStart2.isBefore(t.getEndTime()));
+                if (ok) break;
+            }
+            if (ok) continue;
+            minuteShift = 0;
+            dayShift++;
         }
     }
 
